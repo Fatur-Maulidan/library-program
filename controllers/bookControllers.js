cffebaadd1f01fbed6ec992d4336252781cd8a45
Fatur Maulidan <mfatur757@gmail.com>
2024-08-18 00:36:46 +0700

    fix : Adding try catch at getAllBooks

--- controllers/bookControllers.js
+++ controllers/bookControllers.js
@@ -3,12 +3,19 @@ const connection = require('../config/connection');
 const BooksModel = require('../models/bookModels');
 
 const index = async (req, res) => {
-    const [data] = await BooksModel.getAllBooks();
-    res.json({
-        status: 200,
-        message: 'Success Get All Books',
-        data: data
-    })
+    try{
+        const [data] = await BooksModel.getAllBooks();
+        res.json({
+            status: 200,
+            message: 'Success Get All Books',
+            data: data
+        })
+    } catch(error) {
+        res.status(500).json({
+            message: 'Internal Server Error',
+            errorMessage: error.message
+        });
+    }
 }
 
 module.exports = {
